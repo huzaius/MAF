d11777312e417e0aa6024b0588670fe11cdaa9ab
huzaius <huzaius@gmail.com>
2025-10-07 03:22:06 -0500

    Implemented Votes,Accepted,Sort by

--- qnahub/urls.py
+++ qnahub/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import QuestionCreateView, QuestionListView ,QuestionDetailView, QuestionUpdateView, QuestionDeleteView, QuestionByUserListView, AnswerByUserListView, AnswerUpdateView, AnswerDeleteView
+from .views import QuestionCreateView, QuestionListView ,QuestionDetailView, QuestionUpdateView, QuestionDeleteView, QuestionByUserListView, AnswerByUserListView, AnswerUpdateView, AnswerDeleteView, QuestionVoteView, AnswerVoteView, AcceptAnswerView
 
 
 
@@ -12,6 +12,9 @@ urlpatterns = [
     path('question/<int:pk>/delete/', QuestionDeleteView.as_view(), name='qnahub-question-delete'),
     path('answer/<int:pk>/update/', AnswerUpdateView.as_view(), name='qnahub-answer-update'),
     path('answer/<int:pk>/delete/', AnswerDeleteView.as_view(), name='qnahub-answer-delete'),
+    path('question/<int:pk>/vote/', QuestionVoteView.as_view(), name='qnahub-question-vote'),
+    path('answer/<int:pk>/vote/', AnswerVoteView.as_view(), name='qnahub-answer-vote'),
+    path('answer/<int:pk>/accept/', AcceptAnswerView.as_view(), name='qnahub-answer-accept'),
     # path('user/<str:username>/', QuestionByUserListView.as_view(), name='user-questions'),
     # path('user/<str:username>/answers/', AnswerByUserListView.as_view(), name='user-answers'),    
 ]
--- qnahub/views.py
+++ qnahub/views.py
@@ -1,24 +1,51 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormMixin
+from django.db.models import Max, F, Case, When, DateTimeField, Count
+from django.shortcuts import redirect, get_object_or_404
 
 from qnahub.forms import AnswerForm, QuestionForm
 from .models import Question, Answer
 from django.urls import reverse_lazy
 
 
-
 class QuestionListView(ListView):
     model = Question
     template_name = 'qnahub/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'questions'
-    ordering = ['-timestamp']
     paginate_by = 5
+
+    def get_queryset(self):
+        sort_option = self.request.GET.get('sort', 'newest')
+        queryset = Question.objects.all()
+
+        if sort_option == 'votes':
+            # Annotate with the difference between upvotes and downvotes, then order by it.
+            queryset = queryset.annotate(
+                net_votes_count=Count('upvotes', distinct=True) - Count('downvotes', distinct=True)
+            ).order_by('-net_votes_count', '-timestamp')
+        elif sort_option == 'answers':
+            # Annotate with the count of answers, then order by it.
+            queryset = queryset.annotate(num_answers=Count('answers')).order_by('-num_answers', '-timestamp')
+        else: # Default to 'newest'
+            # Annotate with the latest activity (new answer or new question)
+            queryset = queryset.annotate(latest_answer_time=Max('answers__timestamp'))
+            queryset = queryset.annotate(
+                last_activity=Case(
+                    When(latest_answer_time__isnull=False, then=F('latest_answer_time')),
+                    default=F('timestamp'),
+                    output_field=DateTimeField()
+                )
+            )
+            queryset = queryset.order_by('-last_activity')
+        
+        return queryset
 
 
 class QuestionDetailView(LoginRequiredMixin, FormMixin, DetailView):
     model = Question
     form_class = AnswerForm
+    
 
     def get_success_url(self):
         return reverse_lazy('qnahub-question-detail', kwargs={'pk': self.get_object().pk})
@@ -83,6 +110,74 @@ class AnswerUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         answer = self.get_object()
         return self.request.user == answer.author
+
+class AcceptAnswerView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        answer = get_object_or_404(Answer, pk=pk)
+        question = answer.question
+
+        if request.user != question.author:
+            # This user is not the author of the question, so they can't accept an answer.
+            # We can just redirect them back without doing anything.
+            return redirect('qnahub-question-detail', pk=question.pk)
+
+        if answer.accepted:
+            # If it's already accepted, un-accept it.
+            answer.accepted = False
+            answer.save()
+        else:
+            # Un-accept any other accepted answer for this question first
+            question.answers.filter(accepted=True).update(accepted=False)
+            # Accept the new answer
+            answer.accepted = True
+            answer.save()
+
+        return redirect('qnahub-question-detail', pk=question.pk)
+
+class QuestionVoteView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        question = get_object_or_404(Question, pk=pk)
+        vote_type = request.POST.get('vote_type')
+        user = request.user
+
+        if vote_type == 'up':
+            if user in question.downvotes.all():
+                question.downvotes.remove(user)
+            
+            if user in question.upvotes.all():
+                question.upvotes.remove(user)
+            else:
+                question.upvotes.add(user)
+
+        elif vote_type == 'down':
+            if user in question.upvotes.all():
+                question.upvotes.remove(user)
+
+            if user in question.downvotes.all():
+                question.downvotes.remove(user)
+            else:
+                question.downvotes.add(user)
+        
+        return redirect('qnahub-question-detail', pk=question.pk)
+
+class AnswerVoteView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        answer = get_object_or_404(Answer, pk=pk)
+        vote_type = request.POST.get('vote_type')
+        user = request.user
+
+        # Simplified toggle logic
+        is_upvoted = user in answer.upvotes.all()
+        is_downvoted = user in answer.downvotes.all()
+
+        if vote_type == 'up':
+            answer.upvotes.remove(user) if is_upvoted else answer.upvotes.add(user)
+            if is_downvoted: answer.downvotes.remove(user)
+        elif vote_type == 'down':
+            answer.downvotes.remove(user) if is_downvoted else answer.downvotes.add(user)
+            if is_upvoted: answer.upvotes.remove(user)
+        
+        return redirect('qnahub-question-detail', pk=answer.question.pk)
 
 
 
